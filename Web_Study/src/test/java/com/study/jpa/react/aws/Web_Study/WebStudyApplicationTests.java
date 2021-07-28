340bb67affbb890782515274d9ee8527405af854
pelexsa <pelexsa@naver.com>
2021-07-29 00:20:27 +0900

    리액트 스프링

---
+++ Web_Study/src/test/java/com/study/jpa/react/aws/Web_Study/WebStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jpa.react.aws.Web_Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
