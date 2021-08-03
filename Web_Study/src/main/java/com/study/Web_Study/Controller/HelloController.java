c88a6ad5216926adc8e6d20b3b889a29eecacd2e
pelexsa <pelexsa@naver.com>
2021-08-03 23:02:41 +0900

    모듈화 공부

--- Web_Study/src/main/java/com/study/Web_Study/Controller/HelloController.java
+++ Web_Study/src/main/java/com/study/Web_Study/Controller/HelloController.java
@@ -1,16 +1,31 @@
 package com.study.Web_Study.Controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
 
-@RestController
+@Api(tags = "DemoController")
+@Controller
+@RequestMapping("/template")
 public class HelloController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/api/hello")
     public String hello() {
         return "안녕하세요. 현재 서버 시간은 " + new Date() + "입니다 \n";
+    }
+
+    @ApiOperation("test")
+    @RequestMapping(value="", method = RequestMethod.GET)
+    @ResponseBody
+    public Object getTest(){
+        return null;
     }
 }
